3d0b6da303c9f649a29a876e1a428787e959bff8
Omer Husain <husain.omer@gmail.com>
2022-05-25 17:16:51 -0400

    created first Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="App"><h1>Hello</h1></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>BrainFlix</h1>;
+}
+
+export default Header;
