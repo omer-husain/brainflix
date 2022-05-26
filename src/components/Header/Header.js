8ee5efed71676431131018c07d2c0cfa862099cc
Omer Husain <husain.omer@gmail.com>
2022-05-25 22:53:38 -0400

    built jsx/html structure for header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,20 @@
+
+import logo from "../../assets/logo/BrainFlix-logo.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+
 function Header() {
-  return <h1>BrainFlix</h1>;
+  return (
+    <section className="header">
+      <h1 className="header__heading">BrainFlix</h1>
+      <img className="header__logo" src={logo}></img>
+
+      <form className="header__form">
+        <input className="header__search"></input>
+        <img className="header__avatar" src={avatar}></img>
+        <button className="header__upload-button">UPLOAD</button>
+      </form>
+    </section>
+  );
 }
 
 export default Header;
