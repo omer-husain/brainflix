6282d609f73dccfc8b729b4e60a0d63724302fed
Omer Husain <husain.omer@gmail.com>
2022-06-05 22:44:05 -0400

    improved analytics component styles

--- src/components/VideoAnalytics/VideoAnalytics.js
+++ src/components/VideoAnalytics/VideoAnalytics.js
@@ -4,21 +4,59 @@ import views from "../../assets/icons/views.svg";
 
 export default function VideoAnalytics({ videoData }) {
   return (
-    <section className="videoAnalytics">
-      <label className="videoAnalytics__by-channel">
-        by {videoData.channel}
-      </label>
-      <article className="videoAnalytics__views-container">
-        <img className="videoAnalytics__views-image" src={views}></img>
-        <label className="videoAnalytics__views-data">{videoData.views}</label>
-      </article>
-      <time className="videoAnalytics__date">
-        {new Date(videoData.timestamp).toLocaleDateString()}
-      </time>
-      <article className="videoAnalytics__likes-container">
-        <img className="videoAnalytics__likes-image" src={likes}></img>
-        <label className="videoAnalytics__likes-data">{videoData.likes}</label>
-      </article>
-    </section>
+    <>
+      <section className="videoAnalytics">
+        <label className="videoAnalytics__by-channel">
+          by {videoData.channel}
+        </label>
+        <article className="videoAnalytics__views-container">
+          <img className="videoAnalytics__views-image" src={views}></img>
+          <label className="videoAnalytics__views-data">
+            {videoData.views}
+          </label>
+        </article>
+        <time className="videoAnalytics__date">
+          {new Date(videoData.timestamp).toLocaleDateString()}
+        </time>
+        <article className="videoAnalytics__likes-container">
+          <img className="videoAnalytics__likes-image" src={likes}></img>
+          <label className="videoAnalytics__likes-data">
+            {videoData.likes}
+          </label>
+        </article>
+      </section>
+
+      <section className="videoAnalytics-large">
+        <section className="videoAnalytics-large__column1">
+          <label className="videoAnalytics-large__by-channel">
+            by {videoData.channel}
+          </label>
+
+          <time className="videoAnalytics-large__date">
+            {new Date(videoData.timestamp).toLocaleDateString()}
+          </time>
+        </section>
+        <section className="videoAnalytics-large__column2">
+          <article className="videoAnalytics-large__views-container">
+            <img
+              className="videoAnalytics-large__views-image"
+              src={views}
+            ></img>
+            <label className="videoAnalytics-large__views-data">
+              {videoData.views}
+            </label>
+          </article>
+          <article className="videoAnalytics-large__likes-container">
+            <img
+              className="videoAnalytics-large__likes-image"
+              src={likes}
+            ></img>
+            <label className="videoAnalytics-large__likes-data">
+              {videoData.likes}
+            </label>
+          </article>
+        </section>
+      </section>
+    </>
   );
 }
