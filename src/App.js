aa34396316324cdd3b4e568c787090ea6550ffff
Omer Husain <husain.omer@gmail.com>
2022-06-11 18:57:10 -0400

    changed app.js to functional component as per guidance

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import MainVideoPage from "./pages/MainVideoPage/MainVideoPage";
 import VideoUploadPage from "./pages/VideoUploadPage/VideoUploadPage";
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <section className="App">
-          <Switch>
-            <Route path="/upload" component={VideoUploadPage} />
+function App() {
+  return (
+    <Router>
+      <section className="App">
+        <Switch>
+          <Route path="/upload" component={VideoUploadPage} />
 
-            <Route path="/videos/:videoId" component={MainVideoPage} />
+          <Route path="/videos/:videoId" component={MainVideoPage} />
 
-            <Route path="/" exact component={MainVideoPage} />
-          </Switch>
-        </section>
-      </Router>
-    );
-  }
+          <Route path="/" exact component={MainVideoPage} />
+        </Switch>
+      </section>
+    </Router>
+  );
 }
 
 export default App;
-
